8391daf626f252e89967cf69afcc27fc3fcd414f
Ash <ajifoster3@gmail.com>
2024-03-20 14:24:53 +0000

    KAN-2 Remove coverage time from viewpoint
    To ensure coverageLibrary is concerned only with offline coverage planning

--- utility_libraries/coverage_library/src/CoveragePath.cpp
+++ utility_libraries/coverage_library/src/CoveragePath.cpp
@@ -10,24 +10,3 @@ void CoveragePath::addCoverageViewpoint(const CoverageViewpoint& viewpoint) {
 const std::vector<CoverageViewpoint>& CoveragePath::getPath() const {
     return path;
 }
-
-std::optional<CoverageViewpoint> CoveragePath::getFirstZeroCoverageTimeViewpoint() const {
-    for (const auto& viewpoint : path) {
-        if (viewpoint.getCoverageTime() == std::chrono::seconds(0)) {
-            return viewpoint;
-        }
-    }
-    return std::nullopt; // Return an empty optional if no match is found
-}
-
-void CoveragePath::setFirstZeroCoverageTimeViewpointTime() {
-    auto now = std::chrono::system_clock::now(); // Get the current time
-    auto now_sec = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()); // Convert current time to duration since epoch in seconds
-
-    for (auto& viewpoint : path) {
-        if (viewpoint.getCoverageTime() == std::chrono::seconds(0)) {
-            viewpoint.setCoverageTime(now_sec); // Set the coverage time to the current time in seconds
-            break;
-        }
-    }
-}
