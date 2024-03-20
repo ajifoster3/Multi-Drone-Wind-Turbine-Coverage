89cc8821d7822bf47e977b7cf8b56061b08d6558
Ash <ajifoster3@gmail.com>
2024-03-20 16:32:38 +0000

    KAN-2 Remove unnecessary Viewpoint RobotId

--- utility_libraries/coverage_library/include/CoverageViewpoint.h
+++ utility_libraries/coverage_library/include/CoverageViewpoint.h
@@ -13,17 +13,14 @@ CoverageViewpoint(const Pose& pose, std::chrono::seconds coverageTime, bool assi
 
 Pose getPose() const { return pose_; }
 bool isAssigned() const { return assigned_; }
-int getRobotIDAssigned() const { return robotIDAssigned_; }
 
 void setPose(const Pose& pose) { pose_ = pose; }
 void setAssigned(bool assigned) { assigned_ = assigned; }
-void setRobotIDAssigned(int robotIDAssigned) { robotIDAssigned_ = robotIDAssigned; }
 
 
 private:
     Pose pose_; // Custom Pose
     bool assigned_; // Assigned flag
-    int robotIDAssigned_;
 };
 
 #endif // COVERAGE_VIEWPOINT_HPP
