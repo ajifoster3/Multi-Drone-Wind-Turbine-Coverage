12ed4d9e55a6fd29f472f2f26357d0f63808574a
Ash <ajifoster3@gmail.com>
2024-03-22 14:58:58 +0000

    KAN-3 Add documentation to GreedyIterativeCoveragePathPlanner.h

--- utility_libraries/coverage_library/include/GreedyIterativeCoveragePathPlanner.h
+++ utility_libraries/coverage_library/include/GreedyIterativeCoveragePathPlanner.h
@@ -8,13 +8,35 @@
 #include "CoveragePath.h" // Include the CoveragePath header
 #include "HaversineDistance.h"
 
+/**
+ * @brief Logic for greedy path planning.
+ * 
+ * @details 
+ * Contains the logic for planning coverage paths for a team of 
+ * robots given their starting positions and a list of vector of 
+ * goal viewpoints.
+ * 
+*/
 class GreedyIterativeCoveragePathPlanner {
 public:
+
+    /**
+     * @brief Construct the planner and performs planning.
+     *
+     * @details 
+     * Assigns respective parameters, and plans the coverage path.
+     *
+     * @param robotIDs A list of integers representing the IDs of the robots in the team.
+     * @param initalPoses A list of poses representing the initial poses of the robots. 
+     * @param viewpoints A list of CoverageViewpoints representing the points to be covered.
+    */
     GreedyIterativeCoveragePathPlanner(const std::vector<int>& robotIDs, const std::vector<Pose>& initialPoses, std::vector<CoverageViewpoint>& viewpoints);
 
-    
-
-    // New method to get the paths for each robot
+    /**
+     * @brief Simple getter of coveragePaths.
+     * 
+     * @return A list of coverage paths.
+    */
     std::vector<CoveragePath> getCoveragePaths() const;
 
 private:
@@ -23,7 +45,27 @@ private:
     std::vector<CoverageViewpoint>& viewpoints;
     std::vector<CoveragePath> coveragePaths; // Store a CoveragePath for each robot
 
+    /**
+     * @brief Plans coverage in a iterative greedy manner.
+     * 
+     * @details 
+     * For each robot in the team assign the closest viewpoint,
+     * looping through the robots until all viewpoints are covered,
+     * and assign coveragePaths with the result.
+    */
     void planCoveragePath();
+
+    /**
+     * @brief Returns the index of the closest viewpoint to the given pose.
+     * 
+     * @details 
+     * Uses the haversine distance to find the closest unassigned viewpoint
+     * to the given pose.
+     * 
+     * @param pose The subject pose
+     * 
+     * @return the index of viewpoint assigned
+    */
     int findClosestUnassignedViewpointIndex(const Pose& pose);
 };
 
